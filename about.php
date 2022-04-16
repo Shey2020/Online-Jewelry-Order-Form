1b83aa48f2b6ca173ec0c02ed45f925ad7fa22f3
Shey2020 <68851522+Shey2020@users.noreply.github.com>
2022-04-16 20:31:56 +0800

    Update about.php

--- about.php
+++ about.php
@@ -2,22 +2,21 @@
 
 <h1>About Us</h1>
 <div class="container">
-	<p>"It was so great to hear from you today and it was such weird timing," he said. "This is going to sound funny and a little strange, but you were 
-	in a dream I had just a couple of days ago. I'd love to get together
-	and tell you about it if you're up for a cup of coffee," 
-	he continued, laying the trap he'd been planning for years.</p>
+	<p>Jewellery or jewelry consists of decorative items worn for personal adornment, 
+	such as brooches, rings, necklaces, earrings, pendants, bracelets, and cufflinks. 
+	Jewellery may be attached to the body or the clothes. From a western perspective, 
+	the term is restricted to durable ornaments, excluding flowers for example.</p>
 
-	<p>Generating random paragraphs can be an excellent way for writers to get 
-	their creative flow going at the beginning of the day. The writer has no 
-	idea what topic the random paragraph will be about when it appears. This 
-	forces the writer to use creativity to complete one of three common writing 
-	challenges. The writer can use the paragraph as the first one of a short story 
-	and build upon it. A second option is to use the random paragraph somewhere 
-	in a short story they create. The third option is to have the random paragraph 
-	be the ending paragraph in a short story. No matter which of these challenges 
-	is undertaken, the writer is forced to use creativity to incorporate the 
-	paragraph into their writing.</p>
+	<p>The word jewellery itself is derived from the word jewel, which was anglicised 
+	from the Old French "jouel", and beyond that, to the Latin word "jocale", meaning 
+	plaything. In British English, Indian English, New Zealand English, Hiberno-English, 
+	Australian English, and South African English it is spelled jewellery, while the 
+	spelling is jewelry in American English. Both are used in Canadian English, though 
+	jewelry prevails by a two to one margin. In French and a few other European languages 
+	the equivalent term, joaillerie, may also cover decorated metalwork in precious metal 
+	such as objets d'art and church items, not just objects worn on the person.</p>
 	
+	<p>/wiki</p>
 </div>
 
 <?php include_once('footer.php'); ?>
